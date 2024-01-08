5dfef09be223fa405dd0bdfd6ea0922eda34e057
Robert Bailey <r.bail@hotmail.com>
2024-01-09 07:17:21 +1030

    deploy

--- config/connection.js
+++ config/connection.js
@@ -11,8 +11,11 @@ if(process.env.JAWSDB_URL){
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            host: "localhost",
+            host: "127.0.0.1",
             dialect: "mysql",
+            dialectOptions: {
+              socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+            },
             port: 3306
         }
     );
