2f81d63757b4e7c8adb6caebb93da0f8a3d8d90e
Robert Bailey <r.bail@hotmail.com>
2024-01-09 04:24:26 +1030

    try to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,33 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const mysql2 = require('mysql2'); // Correct package name
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.NODE_ENV === 'production') {
+  // Use Heroku database URL
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    logging: false, // Disable logging to console
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
+  // Use local database credentials
   sequelize = new Sequelize(
-   
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-
     {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-    });
+      logging: true, // Enable logging to console
+    }
+  );
 }
 
 module.exports = sequelize;
