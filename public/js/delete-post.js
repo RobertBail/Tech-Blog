84b8e03b2637d4dcf393bed960414616a14ade51
Robert Bailey <r.bail@hotmail.com>
2024-01-09 23:14:57 +1030

    edit and delete

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -8,7 +8,7 @@ async function deleteFormHandler(event) {
     const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
       body: JSON.stringify({
-        post_id: id
+        post_id
       }),
       headers: {
         'Content-Type': 'application/json'
--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,15 +1,15 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="content"]').value;
 
     //console.log(title);
     //console.log(content);
 
-    //const id = window.location.toString().split('/')[
-      //window.location.toString().split('/').length - 1
-    //];
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
       
       const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
