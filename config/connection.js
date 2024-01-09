ff11d2cd26f0fad3d22d87dcca76bea42bdebafb
Robert Bailey <r.bail@hotmail.com>
2024-01-09 21:20:05 +1030

    trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,7 @@ require("dotenv").config();
 let sequelize;
 
 if(process.env.NODE_ENV === 'production'){
-    sequelize = new Sequelize(process.env.DATABASE_URL,{
-      dialect: 'mysql',
-      logging: false, // Disable logging to console
-      dialectOptions: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      
-    });
+    sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
